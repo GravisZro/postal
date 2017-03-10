c8ab757f0284de7b9b09b3331f16328db76d8038
GravisZro <devnull@localhost>
2017-03-10 05:42:56 -0500

    build now cleans up properly

--- build.sh
+++ build.sh
@@ -249,6 +249,7 @@ c_standard="-std=c11"
 platform=$(to_lower $(uname -s))
 arch=$(uname -m)
 verbose=false
+clean=false;
 makefile="Makefile.gravis"
 build_name="postal"
 backend="sdl2"
@@ -572,9 +573,17 @@ echo "Linker flags: $ldflags"
 echo "Options: $defines"
 echo "Executable: $build_name"
 
-echo "executing: make -f $makefile $build_name -e VERBOSE=\"$verbose\" -e CC=\"$cpp_compiler\" FLAGS=\"$cflags\" LDFLAGS=\"$ldflags\" -e DEFINES=\"$defines\" -e POSTAL_ARCHFLAGS=\"$archflags\" -e POSTAL_BINARY=\"$build_name\" -e BACKEND=\"$backend\" -e CPP_STANDARD=\"$cpp_standard\" -e C_STANDARD=\"$c_standard\" -e BUILD_PATH=\"bin/$(to_lower $platform)${platform_bits}\""
+if [ $clean ]
+then
+make_target="clean"
+else
+make_target="$build_name"
+fi
 
-make -j$makejobs -f $makefile $build_name -e VERBOSE="$verbose" -e CC="$cpp_compiler" FLAGS="$cflags" LDFLAGS="$ldflags" -e DEFINES="$defines" -e POSTAL_ARCHFLAGS="$archflags" -e POSTAL_BINARY="$build_name" -e BACKEND="$backend" -e CPP_STANDARD="$cpp_standard" -e C_STANDARD="$c_standard" -e BUILD_PATH="bin/$(to_lower $platform)${platform_bits}"
+echo "executing: make -f $makefile $make_target -e VERBOSE=\"$verbose\" -e CC=\"$cpp_compiler\" FLAGS=\"$cflags\" LDFLAGS=\"$ldflags\" -e DEFINES=\"$defines\" -e POSTAL_ARCHFLAGS=\"$archflags\" -e POSTAL_BINARY=\"$build_name\" -e BACKEND=\"$backend\" -e CPP_STANDARD=\"$cpp_standard\" -e C_STANDARD=\"$c_standard\" -e BUILD_PATH=\"bin/$(to_lower $platform)${platform_bits}\""
+
+
+make -j$makejobs -f $makefile $make_target -e VERBOSE="$verbose" -e CC="$cpp_compiler" FLAGS="$cflags" LDFLAGS="$ldflags" -e DEFINES="$defines" -e POSTAL_ARCHFLAGS="$archflags" -e POSTAL_BINARY="$build_name" -e BACKEND="$backend" -e CPP_STANDARD="$cpp_standard" -e C_STANDARD="$c_standard" -e BUILD_PATH="bin/$(to_lower $platform)${platform_bits}"
 
 exit 0
 
