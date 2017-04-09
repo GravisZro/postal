ac78361a891a8ff2d4d1c38cf2bfd43bbaa397b7
GravisZro <devnull@localhost>
2017-04-08 20:36:32 -0400

    overhaul resource loading

--- newpix/resourcemanager.h
+++ newpix/resourcemanager.h
@@ -3,37 +3,41 @@
 
 #include "sakarchive.h"
 
+#if !defined(TARGET)
+typedef int32_t milliseconds_t;
+#endif
+
 namespace Resources
 {
   static void normalize_path(std::string& path) noexcept;
 }
 
-template<typename T> // type T must inherit filedataio_t
+template<typename T> // type T must inherit filedata_t
 struct Resource : std::shared_ptr<filedata_t>
 {
   T& operator ->(void) const noexcept
   {
-    ASSERT(*this); // must already be allocated
-    return *reinterpret_cast<T*>(const_cast<uint8_t*>(get()->rawdata.data()));
+    ASSERT(operator bool()); // must already be allocated
+    return *reinterpret_cast<T*>(get()->dataptr);
   }
 
-  virtual Resource<T>& operator=(const std::shared_ptr<filedata_t>& other) noexcept
+  std::shared_ptr<filedata_t>& operator =(std::shared_ptr<filedata_t>& other) noexcept
   {
-    ASSERT(!*this); // must not be previously allocated
     std::shared_ptr<filedata_t>::operator =(other);
-    if(!other->loaded)
-    {
-      static_cast<filedataio_t*>(get())->Load();
-      other->loaded = true;
-    }
-    return *this;
+    static_cast<T*>(get())->load();
+    return other;
   }
 };
 
-
-template<typename T> // type T must inherit filedataio_t
+template<typename T> // type T must inherit filedata_t
 struct MultiResource : std::shared_ptr<filedata_t>
 {
+  enum loop_t : uint16_t
+  {
+    backtofront = 0x0001,
+    fronttoback = 0x0002,
+  };
+
   enum type_t : uint16_t
   {
     stillframe = 0,
@@ -44,40 +48,60 @@ struct MultiResource : std::shared_ptr<filedata_t>
   uint32_t magic;
   uint32_t version;
   type_t   type;
-  uint16_t loopFlags;
-  uint32_t totalTime;
-  uint32_t interval;
-  uint32_t frameCount;
-  std::vector<T*> datapointers;
+  loop_t   loopFlags;
+  milliseconds_t totalTime;
+  milliseconds_t interval;
+  uint32_t       frameCount;
+  std::vector<T> datapointers;
 
-  MultiResource<T>& operator=(const std::shared_ptr<filedata_t>& other) noexcept
+  T& operator [](milliseconds_t time) const noexcept
+  {
+    ASSERT(operator bool()); // must already be allocated
+    if (time >= totalTime) // for playing animation
+    {
+      if (loopFlags & loop_t::backtofront) // loop at the end to the beginning
+        time %= totalTime;
+      else
+        time = totalTime - 1; // last frame
+    }
+    else if (time < 0) // for playing animation in reverse
+    {
+      if (loopFlags & loop_t::fronttoback) // loop at the beginning to the end
+        time = (totalTime - 1) - ((-1 - time) % totalTime);
+      else
+        time = 0; // first frame
+    }
+    ASSERT(time / interval < frameCount); // keep it in range
+    return datapointers[time / interval];
+  }
+
+  std::shared_ptr<filedata_t>& operator =(std::shared_ptr<filedata_t>& other) noexcept
   {
     std::shared_ptr<filedata_t>::operator =(other);
     union
     {
-      T*        dataptrT;
-      uint8_t*  dataptr8;
-      uint16_t* dataptr16;
-      uint32_t* dataptr32;
+      const uint8_t*  dataptr8;
+      const uint16_t* dataptr16;
+      const uint32_t* dataptr32;
     };
 
-    dataptr8  = const_cast<uint8_t*>(other->rawdata.data());
-    magic     = *dataptr32; ++dataptr32;
-    version   = *dataptr32; ++dataptr32;
-    type      = *dataptr16; ++dataptr16;
-    loopFlags = *dataptr16; ++dataptr16;
+    dataptr8  = get()->dataptr;
+    magic     = *dataptr32++;
+    version   = *dataptr32++;
+    type      = *dataptr16++;
+    loopFlags = *dataptr16++;
 
     if(type == stillframe)
     {
       totalTime  = 0;
-      interval   = 0;
+      interval   = 1; // no divide by zero!
       frameCount = 1;
     }
     else
     {
-      totalTime  = *dataptr32; ++dataptr32;
-      interval   = *dataptr32; ++dataptr32;
-      frameCount = *dataptr32; ++dataptr32;
+      totalTime  = *dataptr32++;
+      interval   = *dataptr32++;
+      frameCount = *dataptr32++;
     }
 
     datapointers.resize(frameCount);
@@ -85,46 +109,35 @@ struct MultiResource : std::shared_ptr<filedata_t>
     switch(type)
     {
       case complexanimation:
-        for(uint32_t x : frameCount)
+        for(T& data : datapointers)
         {
-          if(*dataptr32 == UINT32_MAX)
+          if(*dataptr32++ == UINT32_MAX)
           {
-            ++dataptr32;
-            datapointers[x] = dataptrT;
-            ++dataptrT;
-            if(!other->loaded)
-              datapointers[x]->Load();
+            data.dataptr = const_cast<uint8_t*>(dataptr8);
+            data.load();
+            dataptr8 += data.size;
           }
           else
           {
-            ++dataptr32;
-            datapointers[x] = datapointers[*dataptr32];
+            data = datapointers[*dataptr32++];
           }
         }
         break;
 
       case stillframe:
       case animation:
-        for(uint32_t x : frameCount)
+        for(T& data : datapointers)
         {
-          datapointers[x] = dataptrT;
-          ++dataptrT;
-          if(!other->loaded)
-            datapointers[x]->Load();
+          data.dataptr = const_cast<uint8_t*>(dataptr8);
+          data.load();
+          dataptr8 += data.size;
         }
         break;
     }
-
-    other->loaded = true;
-    return *this;
-  }
-
-  T* operator [](uint32_t num) const noexcept
-  {
-    ASSERT(num < frameCount);
-    return dataptr[num];
+    return other;
   }
 };
+
 
 bool rspGetResource(SAKArchive& archive,        // In:  Archive to be used
                     const std::string& filename,           // In:  Resource name
@@ -134,7 +147,7 @@ bool rspGetResource(SAKArchive& archive,        // In:  Archive to be used
     return false;
 
   res = archive.getFile(filename);
-  return res;
+  return res.operator bool();
 }
 
 template <class T>
